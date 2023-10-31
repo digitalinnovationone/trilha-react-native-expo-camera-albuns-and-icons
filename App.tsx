a2a9e6d00f468d687c71b05791a79ca0221a5f29
FelipeAguiarCode <felipe.silva.aguiar047@gmail.com>
2023-10-31 12:20:22 -0300

    project created

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType, FlashMode, AutoFocus } from "expo-camera";
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+//passo 1 - importar
+//passo 2 - configurar
+//passo 3 - utilizar
 
 export default function App() {
+  const [type, setType] = useState<CameraType>(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ratio="16:9"
+        zoom={0}
+        flashMode={FlashMode.off}
+      >
+        <View style={styles.mainView}>
+          <TouchableOpacity
+            style={styles.flipArea}
+            onPress={() => {
+              setType(
+                type === CameraType.back ? CameraType.front : CameraType.back
+              );
+            }}
+          >
+            <Text style={styles.flipText}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -13,8 +40,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "fff",
+  },
+  mainView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  flipArea: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+  },
+  flipText: {
+    fontSize: 20,
+    marginBottom: 15,
+    color: "#FFF",
   },
 });
